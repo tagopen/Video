088c63e8482018b15ada5d390e5cf608df914c1a
Evgeny Bogdanov <admin@prow.su>
2017-11-24 05:43:01 +0300

    Intermediate update

--- app/system/mail/lib/amocrm/amo_route.php
+++ app/system/mail/lib/amocrm/amo_route.php
@@ -12,13 +12,14 @@ require_once(__DIR__."/amocrm.php");
  * @var AmoCRM $amo
  */
 function amo_route($data) {
-    header("Location: https://www.liqpay.ua/api/3/checkout?data=eyJ2ZXJzaW9uIjozLCJhY3Rpb24iOiJwYXkiLCJwdWJsaWNfa2V5IjoiaTc5NDM2NzUxMTIyIiwiYW1vdW50IjoiMTAwIiwiY3VycmVuY3kiOiJVQUgiLCJkZXNjcmlwdGlvbiI6ItCf0L7Qt9C00YDQsNCy0LvQtdC90LjQtSDQtNC70Y8g0L7QtNC90L7Qs9C%2BINGA0LXQsdC10L3QutCwINGB0L4g0YHQutC40LTQutC%2B0LkgNjAlIiwidHlwZSI6ImJ1eSIsImxhbmd1YWdlIjoicnUifQ%3D%3D&signature=HHVfQqB9VnXoj%2Fsr6%2BjuTENpJOQ%3D");
+    $payLink = "https://www.liqpay.ua/api/3/checkout?data=eyJ2ZXJzaW9uIjozLCJhY3Rpb24iOiJwYXkiLCJwdWJsaWNfa2V5IjoiaTc5NDM2NzUxMTIyIiwiYW1vdW50IjoiMTAwIiwiY3VycmVuY3kiOiJVQUgiLCJkZXNjcmlwdGlvbiI6ItCf0L7Qt9C00YDQsNCy0LvQtdC90LjQtSDQtNC70Y8g0L7QtNC90L7Qs9C%2BINGA0LXQsdC10L3QutCwINGB0L4g0YHQutC40LTQutC%2B0LkgNjAlIiwidHlwZSI6ImJ1eSIsImxhbmd1YWdlIjoicnUifQ%3D%3D&signature=HHVfQqB9VnXoj%2Fsr6%2BjuTENpJOQ%3D";
+    header("Location: ".$payLink);
     global $amo;
-    //echo "<pre>";
-    //print_r($data);
+    echo "<pre>";
+    print_r($data);
     $data["host_referer"] = (isset($data["host_referer"]) ? $data["host_referer"] : @(string)$_SERVER["HTTP_REFERER"]);
     if ($amo->auth()) {
-        $contact = $amo->check_contact_entity($data["name"], $data["email"], $data["phone"]);
+        $contact = $amo->check_contact_entity($data["firstname"], $data["email"], $data["phone"]);
         if (!empty($contact)) {
             @parse_str(substr(strstr($data["host_referer"], "?"), 1), $params);
             if (!(is_array($params) && !empty($params))) {
@@ -34,15 +35,15 @@ function amo_route($data) {
                         "id"=>283057,
                         "values"=>array(
                             array(
-                                "value"=>"http://no-link.domain.local"
+                                "value"=>$payLink
                             )
                         )
-                    ), // ID: Ссылка на оплату *** Правка ***
+                    ), // ID: Ссылка на оплату
                     array(
                         "id"=>283129,
                         "values"=>array(
                             array(
-                                "value"=>$data["promocode"]
+                                "value"=>$data["discount"]
                             )
                         )
                     ), // ID: Промокод
@@ -98,10 +99,10 @@ function amo_route($data) {
                         "id"=>283189,
                         "values"=>array(
                             array(
-                                "value"=>@$data["image"]
+                                "value"=>"http://".$_SERVER["HTTP_HOST"]."/".@$data["image"]
                             )
                         )
-                    ), // ID: Фото (ссылка) *** Правка ***
+                    ), // ID: Фото (ссылка)
                     array(
                         "id"=>283151,
                         "values"=>array(
@@ -114,7 +115,7 @@ function amo_route($data) {
                         "id"=>283173,
                         "values"=>array(
                             array(
-                                "value"=>(isset($data["gender-1"]) && ($data["gender-1"] === "" || $data["gender-1"] == 1) ? "Мужской":"Женский")
+                                "value"=>(isset($data["child1"]["gender"]) && ($data["child1"]["gender"] === "" || $data["child1"]["gender"] == 1) ? "Мужской":"Женский")
                             )
                         )
                     ), // ID: Пол 1-го ребенка
@@ -122,7 +123,7 @@ function amo_route($data) {
                         "id"=>283175,
                         "values"=>array(
                             array(
-                                "value"=>(isset($data["new-name-1"]) && $data["new-name-1"] == "on" ? 1 : 0)
+                                "value"=>(isset($data["child1"]["newname"]["trigger"]) && $data["child1"]["newname"]["trigger"] == "on" ? 1 : 0)
                             )
                         )
                     ), // ID: Имя 1-го уникальное
@@ -130,7 +131,7 @@ function amo_route($data) {
                         "id"=>283177,
                         "values"=>array(
                             array(
-                                "value"=>(isset($data["new-name-1"]) && $data["new-name-1"] == "on" ? @$data["child-name-new-1"] : $data["child-name-1"])
+                                "value"=>(isset($data["child1"]["newname"]["trigger"]) && $data["child1"]["newname"]["trigger"] == "on" ? @$data["child1"]["newname"]["name"] : $data["child1"]["name"][(isset($data["child1"]["gender"]) && ($data["child1"]["gender"] === "" || $data["child1"]["gender"] == 1) ? "male":"female")])
                             )
                         )
                     ), // ID: Имя 1-го ребенка
@@ -138,7 +139,7 @@ function amo_route($data) {
                         "id"=>283179,
                         "values"=>array(
                             array(
-                                "value"=>($children == 2 ? (isset($data["gender-2"]) && ($data["gender-2"] === "" || $data["gender-2"] == 1) ? "Мужской":"Женский") : "")
+                                "value"=>($children == 2 ? (isset($data["child2"]["gender"]) && ($data["child2"]["gender"] === "" || $data["child2"]["gender"] == 1) ? "Мужской":"Женский") : "")
                             )
                         )
                     ), // ID: Пол 2-го ребенка
@@ -146,7 +147,7 @@ function amo_route($data) {
                         "id"=>283183,
                         "values"=>array(
                             array(
-                                "value"=>(isset($data["new-name-2"]) && $data["new-name-2"] == "on" ? 1 : 0)
+                                "value"=>(isset($data["child2"]["newname"]["trigger"]) && $data["child2"]["newname"]["trigger"] == "on" ? 1 : 0)
                             )
                         )
                     ), // ID: Имя 2-го уникальное
@@ -154,7 +155,7 @@ function amo_route($data) {
                         "id"=>283187,
                         "values"=>array(
                             array(
-                                "value"=>($children == 2 ? (isset($data["new-name-2"]) && $data["new-name-2"] == "on" ? @$data["child-name-new-2"] : $data["child-name-2"]) : "")
+                                "value"=>($children == 2 ? (isset($data["child2"]["newname"]["trigger"]) && $data["child2"]["newname"]["trigger"] == "on" ? @$data["child2"]["newname"]["name"] : $data["child2"]["name"][(isset($data["child2"]["gender"]) && ($data["child2"]["gender"] === "" || $data["child2"]["gender"] == 1) ? "male":"female")]) : "")
                             )
                         )
                     ), // ID: Имя 2-го ребенка
