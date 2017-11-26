ceadfab8e582cb10700054aa5e3249bbad2fa607
Artem Shcherbakov <Artem2431@gmail.com>
2017-11-26 23:52:29 +0200

    add tagopen settings

--- app/config.php
+++ app/config.php
@@ -7,10 +7,19 @@
   define('DB_PASSWORD', 'dqrgxfj5');
   define('DB_DATABASE', 'shunkin_videpp');
   define('DB_ENCODING', 'utf8');
+  
   //define('DB_DRIVER', 'mysql');
   //define('DB_HOSTNAME', 'localhost');
   //define('DB_USERNAME', 'root');
   //define('DB_PASSWORD', '');
   //define('DB_DATABASE', 'video');
   //define('DB_ENCODING', 'utf8');
+
+  //define('DB_DRIVER', 'mysql');
+  //define('DB_HOSTNAME', 'localhost');
+  //define('DB_USERNAME', 'b18152559_admin');
+  //define('DB_PASSWORD', '7I7k6L7y');
+  //define('DB_DATABASE', 'b18152559_video');
+  //define('DB_ENCODING', 'utf8');
+?>
 ?>
