8b4826fad3e44128a43cb986e875bf9eaacbbdf6
Marchik88 <Marchik88@gmail.com>
2017-12-04 16:57:35 +0200

    4-5dec main page

--- app/js/main.js
+++ app/js/main.js
@@ -190,11 +190,11 @@ Raven.context(function () {
     });
 
     $('#video__modal').on('shown.bs.modal', function() {
-      $("#video__modal .modal__iframe").attr('src', 'https://www.youtube.com/embed/kg-qEHftDd8?ecver=1&autoplay=1&showinfo=0&mute=0&iv_load_policy=3&showsearch=0');
+      $("#video__modal .modal__iframe").attr('src', 'https://www.youtube.com/embed/lLgW4wR1vQQ?ecver=1&autoplay=1&showinfo=0&mute=0&iv_load_policy=3&showsearch=0');
     });
 
     $('#video__modal').on('hidden.bs.modal', function() {
-      $("#video__modal .modal__iframe").attr('src', 'https://www.youtube.com/embed/kg-qEHftDd8?ecver=1&autoplay=0&showinfo=0&mute=1&iv_load_policy=3&showsearch=0');
+      $("#video__modal .modal__iframe").attr('src', 'https://www.youtube.com/embed/lLgW4wR1vQQ?ecver=1&autoplay=0&showinfo=0&mute=1&iv_load_policy=3&showsearch=0');
     });
     
     // Tabs
